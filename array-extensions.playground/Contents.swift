12b4e487cdb9cde5060b4c577bf0005defb617b8
CarloNamoca <carlo.namoca@gmail.com>
2018-10-18 11:22:43 -0700

    First

---
+++ array-extensions.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+
+// Allows you to add numbers regardles if they were Doubles, Floats, etc.
+extension Array where Element: Numeric {
+    func sum() -> Element {
+        return self.reduce(0, {$0 + $1})
+    }
+}
+
+[1, 2, 3].sum()
+
+// We can do this on strings too
+extension Array where Element == String {
+    func concatenate() -> String {
+        return self.reduce("", {$0 + $1 + " "})
+    }
+}
+
+["Hello", "World"].concatenate()
+
+
